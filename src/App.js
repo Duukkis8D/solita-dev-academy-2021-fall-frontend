d4a4e38db51a3027eb0ccb05f0d3f304336b0dbf
Tuukka Virtanen <duukkis@gmail.com>
2021-08-06 04:52:32 +0300

    basic UI elements, no functionality yet

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import React from 'react'
+
 const App = () => {
 
+	return (
+		<div id='appContainer'>
+			<h1>THL vaccine orders and vaccinations</h1>
+			<div id='dateTimePickerContainer'>
+				<label htmlFor='dateTimePicker'>Choose date and time</label>
+				<input type='datetime-local' id='dateTimePicker' name='dateTimePicker'></input>
+			</div>
+			<div id='vaccineInformation'>
+				<h2>Vaccine information</h2>
+			</div>
+		</div>
+	)
 }
 
 export default App
