eb5f7340bb487bf5c520d50b84cbaa0d4dd41755
Tuukka Virtanen <duukkis@gmail.com>
2021-08-12 16:26:22 +0300

    changed baseUrl

--- src/services/orderService.js
+++ src/services/orderService.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/api/orders'
+const baseUrl = '/api/orders'
 
 const getAmountOfVaccines = ( dateAndTime ) => {
 	const request = axios.get( `${baseUrl}/amountOfVaccines/${dateAndTime}` )
